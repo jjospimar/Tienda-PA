c6101ce4d6f8642377ccef9c0bc7fd055c155acd
hfrs0809 <hfrodriguezs@udistrital.edu.co>
2026-09-13 23:42:15 -0500

    Correct syntax and enhance product search
    Fix syntax errors and improve product search function.

--- app/Información.py
+++ app/Información.py
@@ -6,3 +6,12 @@ def mostrar_informacion(productos):
 print(" Productos registrados ")
 for i, producto in enumerate(productos, start=1):
   print(f"{i}. {producto['nombre']} - ${producto['precio']:.2f}")
+  print(f"Total de productos: {len(productos) \n")
+
+def buscar_producto_por_nombre(productos):
+  nombre_buscado = input("Escribe el nombre del producto que quieres buscar: ")
+  for producto in productos:
+        if producto["nombre"].lower() == nombre_buscado.lower():
+            print(f"Encontrado: {producto['nombre']} - ${producto['precio']:.2f}\n")
+            return
+    print(f"No se encontró ningún producto con el nombre '{nombre_buscado}'.\n")
